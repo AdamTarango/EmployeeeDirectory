4b92ae079618f76e49398aafc1e0dc172f062e13
adam tarango <you@example.comadam.tarango97@gmail.comgit commitgit statusgit statusgit config --global user.email you@example.comadam.tarango97@gmail.com>
2024-11-14 15:20:30 -0800

    add get paths

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
 // TODO: this file :)
+const express = require('express')
+const employees = require('./employees')
+const app = express()
+const PORT = 3000;
+
+app.get(('/'), (req, res) => {
+  res.send("Hello Employees!")
+})
+
+app.get(('/employees'), (req, res) => {
+  res.send(employees)
+})
+
+app.get(('/employees/random'), (req, res) => {
+  const number = Math.floor(Math.random() * employees.length)
+  res.send(employees[number])
+})
+
+app.get(('/employees/:id'), (req, res) => {
+  const { id } = req.params;
+  const employee = employees.find((employee) => employee.id === +id)
+  if (employee){
+    res.send(employee)
+  }
+  else {
+    res.send("404 employee not found")
+  }
+})
+
+app.listen(PORT, () =>{
+  console.log(`Listening on port ${PORT}.`)
+})
